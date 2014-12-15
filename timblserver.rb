13b8f11845d0566bf0b4e64c916ef3d0fb7cde89
Folgert Karsdorp <fbkarsdorp@gmail.com>
2014-12-15 14:42:47 +0100

    timblserver (latest) (new formula)
    timblserver (add check)
    
    timblserver (fix sha1)
    
    timbl (fail with clang)
    
    timlserver update fail message (fail with clang)
    
    timblserver (update dependencies)
    
    timblserver (update dependencies)
    
    timblserver (needs openmp)

---
+++ timblserver.rb
@@ -0,0 +1,26 @@
+require "formula"
+
+
+class Timblserver < Formula
+  homepage "http://ilk.uvt.nl/timbl/"
+  url "http://software.ticc.uvt.nl/timblserver-1.9.tar.gz"
+  sha1 "d905034f48d28d3f9bf870021c4389ba3822d4ba"
+
+  depends_on "pkg-config" => :build
+  depends_on "boost"
+  depends_on "timbl"
+  depends_on "ticcutils"
+
+  option "without-check", "skip build-time checks (not recommended)"
+
+  needs :openmp
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "check" if build.with? "check"
+    system "make", "install"
+  end
+end
