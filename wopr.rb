b9dfa24425e50a7bd079e12eff320ce162e369b8
Andrew Janke <janke@eilonwy.local>
2016-08-22 19:31:07 -0500

    wopr 1.36.1

--- wopr.rb
+++ wopr.rb
@@ -1,10 +1,11 @@
 class Wopr < Formula
   homepage "http://ilk.uvt.nl/wopr"
-  url "http://software.ticc.uvt.nl/wopr-1.34.2.tar.gz"
-  sha256 "bc271bcdd92bf28809e0f2e7ef47b739ff69f47c6b974222741b7682250d07be"
+  url "http://software.ticc.uvt.nl/wopr-1.36.1.tar.gz"
+  sha256 "ffcf2ce85f18dcce86a45ecdd9f3066e4c1f8043b8b33bf4bc71bfe91ae32e49"
 
   depends_on "pkg-config" => :build
   depends_on "timbl"
+  depends_on "libfolia"
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
